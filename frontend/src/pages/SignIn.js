e1865330bab6e20c54e0ddf06c554b275d6d6cea
valecoulo <valentinlamothecoulomme@hotmail.com>
2021-12-08 20:50:51 -0300

    feat: formularios front terminados

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -7,8 +7,17 @@ class SignIn extends React.Component {
     render() {
 
         return (
-            <div>
-            <h1 className="text-light">Sign In</h1>
+            <div className="bg-signin">
+                <div id="login-box">
+                    <div class="container-form-signin">
+                        <h1 className="h1-signin">Sign in</h1>
+
+                        <input type="text" name="email" placeholder="E-mail" />
+                        <input type="password" name="password" placeholder="Password" />
+                        
+                        <input type="submit" name="signup_submit" value="Sign in" />
+                    </div>
+                </div>
             </div>
         )
     }
--- frontend/src/pages/signUp.js
+++ frontend/src/pages/signUp.js
@@ -1,17 +1,48 @@
-import React from "react"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
+const SignUp = () => {
 
+  const [ countries, setCountries ] = useState([]);
 
-class SignUp extends React.Component {
-
-    render() {
+  useEffect( async () => {
+    const data = await axios.get('https://restcountries.com/v2/all?fields=name')
+    setCountries(data.data)
+  }, [])
+      
+  console.log("counties pa", countries)
 
         return (
-            <div>
-            <h1 className="text-light">Sign Up</h1>
-            </div>
+         <div className="bg-form">
+            <div id="login-box">
+  <div class="left">
+    <h1 className="signUp-title">Sign up</h1>
+    
+    <input type="text" name="username" placeholder="Username" />
+    <input type="text" name="email" placeholder="E-mail" />
+    <input type="password" name="password" placeholder="Password" />
+    <input type="password" name="password2" placeholder="Retype password" />
+    <select>
+     {countries.map(country => {
+      return <option>{country.name}</option>
+    })}
+    </select>
+    
+    
+    <input type="submit" name="signup_submit" value="Sign me up" />
+  </div>
+  
+  <div class="right">
+    <span class="loginwith">Sign in with<br /></span>
+    
+    <button class="social-signin facebook">Log in with facebook</button>
+    <button class="social-signin google">Log in with Google+</button>
+    <button class="social-signin twitter">Log in with Twitter</button>
+  </div>
+  <div class="or">OR</div>
+</div>
+         </div>
         )
-    }
 }
 
 export default SignUp
